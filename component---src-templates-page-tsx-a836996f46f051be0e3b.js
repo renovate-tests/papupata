0564b4d8763e65d07b55f2c51e2ddefe517483b2
Tapani Haka <tapani.haka@barona.fi>
2020-01-08 15:11:28 +0200

    Updates

---
+++ component---src-templates-page-tsx-a836996f46f051be0e3b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[36],{434:function(n,t,e){"use strict";e.r(t),e.d(t,"query",(function(){return l}));e(1);var r=e(430),a=e(128),d=e(431),u=e(0);t.default=function(n){var t=n.data;return Object(u.d)(d.a,null,Object(u.d)(r.a,null,Object(u.d)(a.a,null,Object(u.d)("h1",null,t.markdownRemark.frontmatter.title),Object(u.d)("div",{dangerouslySetInnerHTML:{__html:t.markdownRemark.html}}))))};var l="1077978455"}}]);
+//# sourceMappingURL=component---src-templates-page-tsx-a836996f46f051be0e3b.js.map

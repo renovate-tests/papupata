d14fbd181d470b87dd51eb009996a8c419dd782f
Tapani Haka <tapani.haka@barona.fi>
2020-01-14 19:10:21 +0200

    Updates

---
+++ component---src-pages-index-tsx-4defc67257d3b541bce3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[34],{467:function(t,e,n){"use strict";n.r(e);n(1);var l=n(431),c=n(129),o=n(432),u=n(0);e.default=function(){return Object(u.d)(o.a,null,Object(u.d)(l.a,null,Object(u.d)(c.a,null,Object(u.d)("h1",null,"Hello!"),Object(u.d)("p",null,"This is the documentation for the papupata typescript library."),Object(u.d)("p",null,"Pick a place to go to from the menu on the left."))))}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-4defc67257d3b541bce3.js.map

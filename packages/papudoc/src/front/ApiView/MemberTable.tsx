3b224e237b2609f86f527602bff2c81448abcf3d
Tapani Haka <tapani.haka@barona.fi>
2020-02-09 22:57:21 +0200

    Add some styles

--- packages/papudoc/src/front/ApiView/MemberTable.tsx
+++ packages/papudoc/src/front/ApiView/MemberTable.tsx
@@ -1,5 +1,7 @@
 import React, { ReactNode } from "react";
 import Description from "./Description";
+import MemberTableTheme from "./MemberTableTheme";
+import styled from "styled-components";
 
 interface Member {
   name: string
@@ -14,20 +16,48 @@ interface Props {
   members: Member[]
 }
 
+const Table = styled.table`
+  font-family: sans-serif;
+  width: 100%;
+`
+
+const Heading = styled.tr`
+  text-align: left;
+  th {
+    padding-right: 15px;
+    border-bottom: 1px solid black;
+  }
+`
+
+const Row = styled.tr<{ even: boolean }>`
+  background: ${props => props.theme.background[props.even ? 'even' : 'odd']};
+  &:hover {
+    background: ${props => props.theme.background.hover[props.even ? 'even' : 'odd']}
+  }
+  > td {
+    border-right: 1px dotted #ccc;
+    border-bottom: 1px dotted #ccc;
+  }
+`
+
 export default function MemberTable({ members }: Props) {
-  return <table>
-    <tr>
-      <th>Name</th>
-      <th>Required</th>
-      <th>Type</th>
-    </tr>
-    {members.map((member, i) => <React.Fragment key={i}><tr>
-      <td>{member.name}</td>
-      <td>{member.required ? 'Yes' : 'No'}</td>
-      <td>{member.type}</td>
-    </tr>
-      {member.description && <tr><td colSpan={3}><Description>{member.description}</Description></td></tr>}
-    </React.Fragment>
-    )}
-  </table>
+  return (
+    <MemberTableTheme>
+      <Table>
+        <Heading>
+          <th>Name</th>
+          <th>Required</th>
+          <th>Type</th>
+        </Heading>
+        {members.map((member, i) => <React.Fragment key={i}><Row even={i % 2 === 0}>
+          <td>{member.name}</td>
+          <td>{member.required ? 'Yes' : 'No'}</td>
+          <td>{member.type}</td>
+        </Row>
+          {member.description && <Row even={i % 2 === 0}><td colSpan={3}><Description>{member.description}</Description></td></Row>}
+        </React.Fragment>
+        )}
+      </Table>
+    </MemberTableTheme>
+  )
 }
---
+++ packages/papudoc/src/front/ApiView/MemberTableTheme.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+import React from "react";
+import { ThemeProvider } from "styled-components";
+
+export default function MemberTableTheme({ children }: { children: ReactNode }) {
+  const theme = {
+    background: {
+      even: '#EEF',
+      odd: '#DDF',
+      hover: {
+        even: '#CCF',
+        odd: '#CCF',
+      }
+    }
+  }
+
+  return <ThemeProvider theme={theme}>
+    {children}
+  </ThemeProvider>
+}

0435236ab54827cf8881221073656e21da7882ef
Tapani Haka <tapani.haka@barona.fi>
2020-02-16 23:00:11 +0200

    Make navbar collapsible

--- docs/src/components/NavMenu/NewNav.tsx
+++ docs/src/components/NavMenu/NewNav.tsx
@@ -1,5 +1,5 @@
 import { NavEntries, NavEntry } from './NavEntries'
-import React from 'react'
+import React, { useState } from 'react'
 import { StaticQuery, graphql, Link } from 'gatsby'
 
 import { Location } from '@reach/router'
@@ -11,8 +11,9 @@ interface Props {
 
 type IsCurrentFn = (url: string) => boolean
 
+const LinkContainer = styled.div``
+
 const NavLink = styled(Link)`
-  display: block;
   ${({ current }: { current: string }) =>
     current === 'true'
       ? `
@@ -25,6 +26,28 @@ const NavLink = styled(Link)`
 const Indent = styled.div`
   margin-left: 20px;
 `
+
+const Toggler = styled.span`
+  display: inline-block;
+  margin: 0 5px 0 -17px;
+  user-select: none;
+  transition: transform 150ms linear;
+  &:hover {
+    color: blue;
+    cursor: pointer;
+  }
+`
+const TogglerSupport = styled.input.attrs({ type: 'checkbox' })`
+  position: absolute;
+  left: -10000px;
+  &:not(:checked) ~ ${Indent} {
+    display: none;
+  }
+  &:checked ~ ${LinkContainer} > ${Toggler} {
+    transform: rotate(90deg);
+  }
+`
+const Container = styled.label``
 
 export default function NewNav({ entries }: Props) {
   return (
@@ -62,7 +85,7 @@ interface EntryProps {
 }
 
 function NewNavEntry({ entry, isCurrent, url }: EntryProps) {
-  const self = (
+  const link = (
     <NavLink current={isCurrent(url) ? 'true' : 'false'} to={url}>
       {typeof entry === 'object' && entry && 'label' in entry ? entry.label : entry}
     </NavLink>
@@ -70,14 +93,37 @@ function NewNavEntry({ entry, isCurrent, url }: EntryProps) {
   const children =
     typeof entry === 'object' && entry && 'children' in entry && entry.children ? (
       <Indent>
-        <NewNavList entries={entry.children as any} isCurrent={isCurrent} />
+        <NewNavList entries={entry.children as NavEntries} isCurrent={isCurrent} />
       </Indent>
     ) : null
 
+  const showChildren = isCurrent(url) || isAChildSelected(entry, isCurrent)
+  const [checked, setChecked] = useState(showChildren)
+
   return (
-    <>
-      {self}
+    <Container>
+      <TogglerSupport checked={checked} />
+      <LinkContainer>
+        {children && (
+          <Toggler
+            onClick={e => {
+              e.preventDefault()
+              setChecked(s => !s)
+            }}
+          >
+            {'>'}
+          </Toggler>
+        )}
+        {link}
+      </LinkContainer>
       {children}
-    </>
+    </Container>
   )
+}
+
+function isAChildSelected(entry: NavEntry, isCurrent: IsCurrentFn): boolean {
+  if (typeof entry !== 'object' || !entry || !('children' in entry)) return false
+  return Object.entries(entry.children as NavEntries).some(child => {
+    return isCurrent(child[0]) || isAChildSelected(child[1], isCurrent)
+  })
 }

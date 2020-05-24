6fc1659cefb78f6b2985c9521c96bde9a338182a
Tapani Haka <tapani.haka@barona.fi>
2020-05-24 20:17:39 +0300

    Highlight selected API

--- packages/dev-client/src/views/APIView/NavAPI.tsx
+++ packages/dev-client/src/views/APIView/NavAPI.tsx
@@ -1,29 +1,39 @@
 import { API } from '../../typedAPI'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import React, { useCallback } from 'react'
 import navigate from '../../navigate'
+import { useRoute } from 'react-router5'
 
 interface Props {
   api: API
 }
 
 const Container = styled.div``
-const Link = styled.a`
+const Link = styled.a<{ selected: boolean }>`
   color: blue;
   cursor: pointer;
   &:hover {
     text-decoration: underline;
   }
+  ${({ selected }) =>
+    !selected
+      ? ''
+      : css`
+          font-weight: bold;
+        `}
 `
 
 export default function NavAPI({ api }: Props) {
+  const selectedAPI = useRoute().route?.params?.apiName
   const nameParts = api.name.split('.')
   const myName = nameParts[nameParts.length - 1]
 
   const access = useCallback(() => navigate.toAPI(api.name), [api])
   return (
     <Container>
-      <Link onClick={access}>{myName}</Link>
+      <Link selected={selectedAPI === api.name} onClick={access}>
+        {myName}
+      </Link>
     </Container>
   )
 }

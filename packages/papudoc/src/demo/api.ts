887b12632292d24a52018212a72c80e9c7d8accf
Tapani Haka <tapani.haka@barona.fi>
2020-05-24 19:36:27 +0300

    More minor changes/fixes

--- packages/papudoc/src/demo/api.ts
+++ packages/papudoc/src/demo/api.ts
@@ -1,61 +1,58 @@
-import { APIDeclaration } from "papupata";
-import { papudoc } from "../papudoc";
+import { APIDeclaration } from 'papupata'
+import { papudoc } from '../papudoc'
+import { Request } from 'express'
 
-const decl = new APIDeclaration();
+const decl = new APIDeclaration<Request>()
 
 interface InterfaceHere {
-  key: string;
+  key: string
   thisIsInterfaceHere: boolean
 }
 
 enum MyEnum {
   alpha,
   beta,
-  gamma
+  gamma,
 }
 const api = {
   testGet1: decl.declareGetAPI('/test-get').response<string[]>(),
   nested: {
-    testGet2: decl.declareGetAPI('/test-get').response<{ key: string, value: number }>(),
+    testGet2: decl.declareGetAPI('/test-get').response<{ key: string; value: number }>(),
   },
 
-  /** 
+  /**
    * @description This API does a lot of things, none of which are useful.
-   * @param id This is the id for the thing 
-  *  @param gamma is also kinda cool  
-  * */
+   * @param id This is the id for the thing
+   *  @param gamma is also kinda cool
+   * */
   testGet3: decl
-    .declareGetAPI("/test-get/:id")
-    .params([
-      "id",
-      "alsodesc",
-      "nodesc"
-    ] as const)
-    .query(["alpha"] as const)
-    .optionalQuery(["gamma"] as const)
-    .queryBool(["beta"] as const)
+    .declareGetAPI('/test-get/:id')
+    .params(['id', 'alsodesc', 'nodesc'] as const)
+    .query(['alpha'] as const)
+    .optionalQuery(['gamma'] as const)
+    .queryBool(['beta'] as const)
     .body<{
       /** @description This is a number. */
-      number: number;
-      string: string;
-      boolean: boolean;
-      undefined: undefined;
-      unknown: unknown;
-      null: null;
-      never: never;
-      conststring: "conststring";
-      constnumber: 999;
-      union: string | number;
-      inlineinterface: { key: string };
-      stringarray: string[];
-      enumref: MyEnum;
-      namedinterface: InterfaceHere;
-      optionalstring?: string;
-      constboolean: false;
-      intersection: { key: string } & { another: number };
-      tuple: [string, number, boolean];
-      method(): void;
-      function: () => void;
+      number: number
+      string: string
+      boolean: boolean
+      undefined: undefined
+      unknown: unknown
+      null: null
+      never: never
+      conststring: 'conststring'
+      constnumber: 999
+      union: string | number
+      inlineinterface: { key: string }
+      stringarray: string[]
+      enumref: MyEnum
+      namedinterface: InterfaceHere
+      optionalstring?: string
+      constboolean: false
+      intersection: { key: string } & { another: number }
+      tuple: [string, number, boolean]
+      method(): void
+      function: () => void
       deeper: {
         has: {
           a: {
@@ -64,23 +61,16 @@ const api = {
         }
       }
     }>()
-    .response<Array<Omit<{ key: string; value: number }, "key">>[]>()
-  ,
+    .response<Array<Omit<{ key: string; value: number }, 'key'>>[]>(),
   deep: {
-    testPost: decl
-      .declareGetAPI("/test-get")
-      .body<InterfaceHere>()
-      .response<string>()
-  }
-};
+    testPost: decl.declareGetAPI('/test-get').body<InterfaceHere>().response<string>(),
+  },
+}
 
-papudoc(api);
+papudoc(api)
 
 export const extra = {
   deep: {
-    testPost: decl
-      .declareGetAPI("/test-get")
-      .body<InterfaceHere>()
-      .response<string>()
-  }
-};
+    testPost: decl.declareGetAPI('/test-get').body<InterfaceHere>().response<string>(),
+  },
+}
